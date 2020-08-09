46c7e3da12bca7f4e1eb5b8733aa262f5fee00d0
김민섭 <unknown>
2020-08-09 14:29:53 +0900

    CSS)검색영역 텍스트필드, 검색아이콘, 목록버튼 추가

--- src/main/java/com/ms/nas/api/controller/HelloController.java
+++ src/main/java/com/ms/nas/api/controller/HelloController.java
@@ -3,12 +3,17 @@ package com.ms.nas.api.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
 import java.util.Date;
 
 @RestController
 public class HelloController {
     @GetMapping("/api/hello")
     public String hello(){
-        return "안녕하세요. 현재 서버시간은 "+new Date() +"입니다. \n";
+
+        File file = new File("/volume1/animation");
+        System.err.println(file.isDirectory());
+
+        return "/volume1/animation 디렉토리 체크 : " + file.isDirectory();
     }
 }
