e65f46d79550eafcb344f07792255dc0bee45527
김민섭 <unknown>
2020-08-09 23:10:31 +0900

    CSS)전체목록 트리 트리 컴포넌트 추가

--- frontend/src/container/TreeListScreen/TreeListScreen.jsx
+++ frontend/src/container/TreeListScreen/TreeListScreen.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import SearchScreen from '../SearchScreen/SearchScreen'
+import SearchScreen from '../SearchScreen/SearchScreen';
+import Tree from '../../component/Tree/Tree';
 
 export default function TreeListScreen() {
     return (
         <div style={{ padding: '10px' }}>
             <SearchScreen />
-            <div>tree list 들어 갈곳.</div>
+            <Tree />
         </div>
     )
 }
