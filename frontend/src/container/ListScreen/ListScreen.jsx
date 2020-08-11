7b29d4199ef63b7af1a36eedf9f52e6e2f799bb4
김민섭 <unknown>
2020-08-11 12:43:04 +0900

    CSS)업데이트 리스트 리스트 컴포넌트 추가

---
+++ frontend/src/container/ListScreen/ListScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import List from '../../component/List/List';
+
+export default function ListScreen() {
+
+    return (
+        <>
+            <List />
+        </>
+    )
+}
