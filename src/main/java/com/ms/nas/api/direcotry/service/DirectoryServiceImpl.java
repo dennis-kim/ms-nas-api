0464f2cd74fd867f4284969d2916951e5430fe87
DESKTOP-JTRL0QG\kms <seidin1@naver.com>
2020-08-16 03:31:48 +0900

    Tree data 조회 #13 제외 디렉토리 추가

--- src/main/java/com/ms/nas/api/direcotry/service/DirectoryServiceImpl.java
+++ src/main/java/com/ms/nas/api/direcotry/service/DirectoryServiceImpl.java
@@ -31,6 +31,8 @@ public class DirectoryServiceImpl implements DirectoryService{
     private final String pattern = "yyyy-MM-dd hh:mm aa";
     private SimpleDateFormat simpleDateFormat;
 
+    private final String[] exceptDirectories = {"@eaDir", ".DS_Store"};   // 제외 디렉토리
+
     @Override
     public List<DirectoryVO> getDirectories() {
         if(simpleDateFormat == null) {
@@ -55,7 +57,7 @@ public class DirectoryServiceImpl implements DirectoryService{
     private DirectoryVO getDirectoryVO(String path) {
         DirectoryVO vo;
         File directory = new File(path);
-        if(directory.isDirectory()) {
+        if(directory.isDirectory() && this.isTargetDirectory(directory.getName())) {
             vo = new DirectoryVO();
             vo.setDirName(directory.getName());
             vo.setDirPath(path);
@@ -71,12 +73,14 @@ public class DirectoryServiceImpl implements DirectoryService{
 
         if(file.isDirectory()) {
             for(File child : file.listFiles()) {
-                DirectoryVO vo = new DirectoryVO();
-                vo.setDirName(child.getName());
-                vo.setDirPath(child.getPath());
-                vo.setUpdateDate(getUpdateDate(child.lastModified()));
-                vo.setChild(getChild(child));
-                childVO.add(vo);
+                if(this.isTargetDirectory(child.getName())) {
+                    DirectoryVO vo = new DirectoryVO();
+                    vo.setDirName(child.getName());
+                    vo.setDirPath(child.getPath());
+                    vo.setUpdateDate(getUpdateDate(child.lastModified()));
+                    vo.setChild(getChild(child));
+                    childVO.add(vo);
+                }
             }
         }
         return childVO;
@@ -91,6 +95,22 @@ public class DirectoryServiceImpl implements DirectoryService{
         Date lastModifiedDate = new Date( lastModified );
         String updateDate = simpleDateFormat.format(lastModifiedDate);
         return updateDate;
+    }
+
+    /**
+     * 대상 디렉토리 인지 체크
+     * exceptDirectories 항목에 있는 디렉토리는 표시하지 않음.
+     * @param directoryName
+     * @return true : 사용가능 디렉토리
+     *         false : 제외 디렉토리
+     */
+    private boolean isTargetDirectory(String directoryName) {
+        for(String except : exceptDirectories) {
+            if(except.equals(directoryName)) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
