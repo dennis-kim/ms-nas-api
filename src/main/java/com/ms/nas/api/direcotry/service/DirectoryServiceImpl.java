ecb43bd0138d3686c831d2b5cc43ea972c28c8a0
DESKTOP-JTRL0QG\kms <seidin1@naver.com>
2020-08-16 03:15:18 +0900

    Tree data 조회 #13 트리 데이터 조회 개발 완료

--- src/main/java/com/ms/nas/api/direcotry/service/DirectoryServiceImpl.java
+++ src/main/java/com/ms/nas/api/direcotry/service/DirectoryServiceImpl.java
@@ -4,44 +4,67 @@ import com.ms.nas.api.direcotry.vo.DirectoryVO;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class DirectoryServiceImpl implements DirectoryService{
 
-    private final String BASE_PATH = "d:/directory/";// volume 위치가 될곳
+    //private final String BASE_PATH1 = "d:/directory/";// volume 위치가 될곳
+    //private final String BASE_PATH2 = "d:/directory/";// volume 위치가 될곳
 
-    private final String MOVIE = "movie";
-    private final int lastDepthForMovie = 3;
+    private final String BASE_PATH1 = "/volume1/";// volume 위치가 될곳
+    private final String BASE_PATH2 = "/volume2/";// volume 위치가 될곳
 
+    private final String COMIX = BASE_PATH1 + "만화책";
+    private final String DRAMA = BASE_PATH1 + "drama";
+    private final String DOCUMENTARY = BASE_PATH1 + "documentary";
+    private final String ANIMATION = BASE_PATH1 + "animation";
 
-    private final String VARIETY = "예능";
+    private final String MOVIE = BASE_PATH2 + "movie";
+    private final String VARIETY = BASE_PATH2 + "예능";
 
+    private String[] mainDirectories = { MOVIE, VARIETY, COMIX, DRAMA, DOCUMENTARY, ANIMATION };
 
-
+    private final String pattern = "yyyy-MM-dd hh:mm aa";
+    private SimpleDateFormat simpleDateFormat;
 
     @Override
     public List<DirectoryVO> getDirectories() {
+        if(simpleDateFormat == null) {
+            simpleDateFormat = new SimpleDateFormat(pattern);
+        }
+
         List<DirectoryVO> list = new ArrayList<>();
-        list.add(getUnderDirectories());
+        for(String path : mainDirectories) {
+            DirectoryVO vo = getUnderDirectories(path);
+            if(vo != null) {
+                list.add(vo);
+            }
+        }
 
         return list;
     }
 
-    private DirectoryVO getUnderDirectories() {
-        return this.getDirectoryVO();
+    private DirectoryVO getUnderDirectories(String path) {
+        return this.getDirectoryVO(path);
     }
 
-    private DirectoryVO getDirectoryVO() {
-        DirectoryVO movieVO = new DirectoryVO();
-        String moviePath = BASE_PATH + MOVIE;
-        movieVO.setDirName(MOVIE);
-        movieVO.setDirPath(moviePath);
-        movieVO.setChild(this.getChild(new File(moviePath)));
-        return movieVO;
+    private DirectoryVO getDirectoryVO(String path) {
+        DirectoryVO vo;
+        File directory = new File(path);
+        if(directory.isDirectory()) {
+            vo = new DirectoryVO();
+            vo.setDirName(directory.getName());
+            vo.setDirPath(path);
+            vo.setUpdateDate(getUpdateDate(directory.lastModified()));
+            vo.setChild(this.getChild(directory));
+            return vo;
+        }
+        return null;
     }
-
 
     private List<DirectoryVO> getChild(File file) {
         List<DirectoryVO> childVO = new ArrayList<>();
@@ -51,19 +74,23 @@ public class DirectoryServiceImpl implements DirectoryService{
                 DirectoryVO vo = new DirectoryVO();
                 vo.setDirName(child.getName());
                 vo.setDirPath(child.getPath());
+                vo.setUpdateDate(getUpdateDate(child.lastModified()));
                 vo.setChild(getChild(child));
                 childVO.add(vo);
             }
         }
-
         return childVO;
     }
 
-
-
-
-
-
-
+    /**
+     * 디렉토리 마지막 수정시간 조회
+     * @param lastModified ( File(path).lastModified() )
+     * @return
+     */
+    private String getUpdateDate(long lastModified) {
+        Date lastModifiedDate = new Date( lastModified );
+        String updateDate = simpleDateFormat.format(lastModifiedDate);
+        return updateDate;
+    }
 
 }
